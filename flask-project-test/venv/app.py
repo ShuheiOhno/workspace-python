b7265571cd0a46a747bc7310f5dffbe28b5db5d5
ShuheiOhno <57937334+ShuheiOhno@users.noreply.github.com>
2023-04-29 18:17:15 +0900

    1

--- flask-project-test/venv/app.py
+++ flask-project-test/venv/app.py
@@ -3,6 +3,14 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/hello')
 def hello():
-    return 'Hello, World!!!'
+    return 'Hello, World!!!!'
+
+@app.route('/hello2')
+def hello2():
+    return 'Hello, World2'
+
+@app.route('/hello3')
+def hello3():
+    return 'Hello, World!3'
