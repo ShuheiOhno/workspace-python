c285d5ece61a0523cdc9b9e3bce8ab97aaaf8039
ShuheiOhno <57937334+ShuheiOhno@users.noreply.github.com>
2023-04-29 19:35:47 +0900

    d

--- flask_db/app.py
+++ flask_db/app.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql:///blog.db'
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String)
 
 @app.route('/')
 def index():
