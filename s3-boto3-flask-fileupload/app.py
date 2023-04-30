7ed8679f6942f7b4d622dd1740d79dc3b5fa4110
ShuheiOhno <57937334+ShuheiOhno@users.noreply.github.com>
2023-04-30 15:40:22 +0900

    f

---
+++ s3-boto3-flask-fileupload/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+db = SQLAlchemy(app)
+
+class File(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    original_filname = db.Column(db.String(100))
+    filename = db.Column(db.String(100))
+    bucket = db.Column(db.String(100))
+    region = db.Column(db.String(100))
+
