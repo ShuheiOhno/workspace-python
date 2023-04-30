bf796a8c14467916263b41cf820d77d71a8880df
ShuheiOhno <57937334+ShuheiOhno@users.noreply.github.com>
2023-04-30 15:54:51 +0900

    r

--- s3-boto3-flask-fileupload/app.py
+++ s3-boto3-flask-fileupload/app.py
@@ -1,6 +1,11 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png'}
+
+def allowd_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
@@ -16,4 +21,10 @@ class File(db.Model):
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        uploaded_file = request.files["file-to-save"]
+        return redirect(url_for("index"))
+    
+    files = File.query.all()
+
+    return render_template("index.html", files=files)
