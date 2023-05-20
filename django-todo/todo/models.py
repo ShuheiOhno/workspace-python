05ff05b6f0ef31860bfd3e9af782275a8abf3422
ShuheiOhno <57937334+ShuheiOhno@users.noreply.github.com>
2023-05-20 23:00:43 +0900

    f

--- django-todo/todo/models.py
+++ django-todo/todo/models.py
@@ -6,6 +6,7 @@ class TodoModel(models.Model):
     title = models.CharField(max_length=100)
     memo = models.TextField()
 
+    # 管理画面の表示をタイトルにする
     def __str__(self):
         return self.title
 
