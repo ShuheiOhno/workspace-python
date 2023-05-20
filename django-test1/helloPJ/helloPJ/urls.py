036f47dd32bc28880325b7c57526a44b53599963
ShuheiOhno <57937334+ShuheiOhno@users.noreply.github.com>
2023-05-20 21:16:57 +0900

    s

--- django-test1/helloPJ/helloPJ/urls.py
+++ django-test1/helloPJ/helloPJ/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import hellofunction
+from .views import hellofunction, checkview
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', hellofunction),
+    path('check/', checkview),
 ]
--- django-test1/helloPJ/helloPJ/views.py
+++ django-test1/helloPJ/helloPJ/views.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 
@@ -9,3 +10,13 @@ def hellofunction(request):
 # クラスベース
 class HelloWorldClass(TemplateView):
     template_name = 'hello.html'
+
+# 確認用
+def checkview(request):
+    print('##################')
+    print(Path()) #.
+    print(Path(__file__)) #このファイルのパス
+    print(Path(__file__).resolve()) #このファイルの絶対パス
+    print(Path(__file__).resolve().parent) #このファイルの一つ上のフォルダ
+    print(Path(__file__).resolve().parent.parent) #BASE_DIRと同じ
+    return HttpResponse('')
