30a7899c365d25346b9b94574270554c6713a1c9
ShuheiOhno <57937334+ShuheiOhno@users.noreply.github.com>
2023-05-21 00:14:57 +0900

    f

--- django-todo/todo/urls.py
+++ django-todo/todo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import TodoList, TodoDetail
+from .views import TodoList, TodoDetail, TodoCreate
 
 urlpatterns = [
-    path('list/', TodoList.as_view()), #クラスの場合、as_view()を付ける
-    path('detail/<int:pk>', TodoDetail.as_view()),
+    path('list/', TodoList.as_view(), name='list'), #クラスの場合、as_view()を付ける
+    path('detail/<int:pk>', TodoDetail.as_view(), name='detail'),
+    path('create/', TodoCreate.as_view(), name='create'),
 ]
--- django-todo/todo/views.py
+++ django-todo/todo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import TodoModel
+from django.urls import reverse_lazy
 
 # Create your views here.
 class TodoList(ListView):
@@ -10,3 +11,9 @@ class TodoList(ListView):
 class TodoDetail(DetailView):
     template_name = 'detail.html'
     model = TodoModel
+
+class TodoCreate(CreateView):
+    template_name = 'create.html'
+    model = TodoModel #保存するモデルを指定
+    fields = ('title', 'memo', 'priority', 'duedate')
+    success_url = reverse_lazy('list') #保存後の遷移先 urls.pyのpathの第二引数に、name='list'を指定
