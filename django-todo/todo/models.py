53e608b7afb284364e4ce223b0a3d7b832665243
ShuheiOhno <57937334+ShuheiOhno@users.noreply.github.com>
2023-05-20 23:51:02 +0900

    d

--- django-todo/todo/models.py
+++ django-todo/todo/models.py
@@ -2,9 +2,16 @@ from django.db import models
 
 # Create your models here.
 
+CHOICE = (('danger', 'high'), ('warning','normal'), ('primary','low'))
+
 class TodoModel(models.Model):
     title = models.CharField(max_length=100)
     memo = models.TextField()
+    priority = models.CharField(
+        max_length=50,
+        choices=CHOICE
+    )
+    duedate = models.DateField()
 
     # 管理画面の表示をタイトルにする
     def __str__(self):
