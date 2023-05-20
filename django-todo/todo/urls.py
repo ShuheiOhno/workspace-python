37a73b31d6649900e610a4f99d6e82c2de5d2ca6
ShuheiOhno <57937334+ShuheiOhno@users.noreply.github.com>
2023-05-20 23:09:46 +0900

    d

--- django-todo/todo/urls.py
+++ django-todo/todo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import TodoList
+from .views import TodoList, TodoDetail
 
 urlpatterns = [
     path('list/', TodoList.as_view()), #クラスの場合、as_view()を付ける
+    path('detail/<int:pk>', TodoDetail.as_view()),
 ]
