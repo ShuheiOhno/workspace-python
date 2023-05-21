7e7e9d4a8f114f0db8b381a2261cd616b50fa42e
ShuheiOhno <57937334+ShuheiOhno@users.noreply.github.com>
2023-05-21 10:18:41 +0900

    Update urls.py

--- django-sns/boardapp/urls.py
+++ django-sns/boardapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from .views import rendertest ,signupfunc, loginfunc, listfunc
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # テスト用
@@ -8,4 +10,4 @@ urlpatterns = [
     path('signup/', signupfunc, name='signup'),
     path('login/', loginfunc, name='login'),
     path('list/', listfunc ,name='list'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
