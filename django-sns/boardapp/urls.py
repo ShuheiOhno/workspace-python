ac5cad9d113f5f02ca57350d2905d9eb845d884b
ShuheiOhno <57937334+ShuheiOhno@users.noreply.github.com>
2023-05-21 12:09:39 +0900

    ｆ

--- django-sns/boardapp/urls.py
+++ django-sns/boardapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import rendertest ,signupfunc, loginfunc, listfunc, logoutfunc, detailfunc
+from .views import rendertest ,signupfunc, loginfunc, listfunc, logoutfunc, detailfunc, goodfunc
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -12,5 +12,6 @@ urlpatterns = [
     path('list/', listfunc ,name='list'),
     path('logout/', logoutfunc, name='logout'),
     path('detail/<int:pk>', detailfunc, name='detail'),
+    path('good/<int:pk>', goodfunc, name='good'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
 + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
