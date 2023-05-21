1b3480dee85c7026aa673a98d073d0d397e34a70
ShuheiOhno <57937334+ShuheiOhno@users.noreply.github.com>
2023-05-21 09:45:13 +0900

    ｇ

--- django-sns/boardapp/urls.py
+++ django-sns/boardapp/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
-from .views import rendertest ,signupfunc, loginfunc
+from .views import rendertest ,signupfunc, loginfunc, listfunc
 
 urlpatterns = [
     # テスト用
     path('rendertest/', rendertest),
 
-    path('signup/', signupfunc),
-    path('login/', loginfunc)
-
+    path('signup/', signupfunc, name='signup'),
+    path('login/', loginfunc, name='login'),
+    path('list/', listfunc ,name='list'),
 ]
--- django-sns/boardapp/views.py
+++ django-sns/boardapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import authenticate, login
@@ -30,3 +30,6 @@ def loginfunc(request):
         else:
             return render(request, 'login.html', {'context': 'not login'})
     return render(request, 'login.html', {'context': 'get'})
+
+def listfunc(request):
+    return render(request, 'list.html')
