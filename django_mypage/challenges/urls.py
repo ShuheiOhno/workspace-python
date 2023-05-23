9ba9fcd2e96db2a7be6aecc3e8e702ac608064a1
ShuheiOhno <57937334+ShuheiOhno@users.noreply.github.com>
2023-05-23 22:14:54 +0900

    dynamic

--- django_mypage/challenges/urls.py
+++ django_mypage/challenges/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("january/", views.index),
-    path("february/", views.february)
+    path("<str:month>/", views.monthly_challenge),
 ]
--- django_mypage/challenges/views.py
+++ django_mypage/challenges/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Work!")
+# def index(request):
+#     return HttpResponse("Work!")
 
-def february(request):
-    return HttpResponse("Fabruary")
+# def february(request):
+#     return HttpResponse("Fabruary")
+
+def monthly_challenge(request, month):
+    challenge_text = None
+    print(request)
+    if month == "january":
+        challenge_text = "january"
+    elif month == "february":
+        challenge_text = "february"
+        print('aaa')
+    elif month == "march":
+        challenge_text = "march"
+    else:
+        return HttpResponseNotFound("This month is not supported.")
+
+    return HttpResponse(challenge_text)
