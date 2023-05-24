2fb3e81bddbbaeb34753d3459d574dcb8708c5de
ShuheiOhno <57937334+ShuheiOhno@users.noreply.github.com>
2023-05-25 07:00:42 +0900

    1

--- django_mypage/challenges/views.py
+++ django_mypage/challenges/views.py
@@ -8,6 +8,24 @@ from django.http import HttpResponse, HttpResponseNotFound
 # def february(request):
 #     return HttpResponse("Fabruary")
 
+monthly_challenges = {
+    "january": "1",
+    "february": "2",
+    "march": "3",
+    "april": "4",
+    "may": "5",
+    "june": "6",
+    "july": "7",
+    "august": "8",
+    "september": "9",
+    "october": "10",
+    "november": "11",
+    "december": "12",
+}
+
+def monthly_chanllenge_by_number(request, month):
+    return HttpResponse(month)
+
 def monthly_challenge(request, month):
     challenge_text = None
     print(request)
