4a4daece47737f5f4e7b8d18a753dee4e6326167
ShuheiOhno <57937334+ShuheiOhno@users.noreply.github.com>
2023-08-11 11:08:50 +0900

    Update app.py

--- flask_basic/app.py
+++ flask_basic/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 app = Flask(__name__) #アプリケーションオブジェクト __name__：現在のファイルのモジュール名が入る
 
+#View関数
 @app.route('/')
 def index():
     return '<h1>Hello World</h1>'
@@ -9,6 +10,10 @@ def index():
 @app.route('/test')
 def test():
     return 'aaa'
+
+@app.route('/user/<user_id>')
+def userid(user_id):
+    return '<h1>ID:{0}</h1>'.format(user_id)
 
 if __name__ == '__main__': #このファイルをスクリプトとして実行した場合(インポートされた場合の実行を防ぐため、など)
     app.run()
