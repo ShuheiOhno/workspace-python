3068a5e5feba47678d233904acfadc6174c0dc82
ShuheiOhno <57937334+ShuheiOhno@users.noreply.github.com>
2023-08-11 11:15:24 +0900

    Update app.py

--- flask_basic/app.py
+++ flask_basic/app.py
@@ -14,7 +14,9 @@ def test():
 @app.route('/user/<user_id>')
 def userid(user_id):
     return '<h1>ID:{0}</h1>'.format(user_id)
+    # return '<h1>ID  :{0} {1} {2}</h1>'.format(user_id[0],user_id[1],user_id[2]) #error
 
 if __name__ == '__main__': #このファイルをスクリプトとして実行した場合(インポートされた場合の実行を防ぐため、など)
+    # app.run(debug=True)
     app.run()
 
