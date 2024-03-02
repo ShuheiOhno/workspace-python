dbe9b051bb937f0825bdeb2ca234d7eab3cc2b2a
ShuheiOhno <57937334+ShuheiOhno@users.noreply.github.com>
2024-03-02 22:03:08 +0900

    Update validate_number.py

--- jissen/3/validate_number.py
+++ jissen/3/validate_number.py
@@ -1,4 +1,4 @@
-class MyValidateError(Exception)
+class MyValidateError(Exception):
     title = None
     detail = None
 
@@ -6,4 +6,25 @@ class MyValidateError(Exception)
 
         return str(self.title)
     
-    class My
+class MyTypeError(MyValidateError):
+    title="Type error"
+    detail="数値で入力してください"
+
+class MyMaxError(MyValidateError):
+    title="Max error"
+    detail="Max値100までの値を入力してください"
+
+def validate_number(num):
+    try:
+        num=int(num)
+    except ValueError:
+        raise MyTypeError
+    if num > 100:
+        raise MyMaxError
+    
+try:
+    input_number=input("検証する数字を入力してください=>")
+    validate_number(input_number)
+except MyValidateError as e:
+    print(f'{e}の例外が発生しました')
+    print(f'detail={e.detail}')
